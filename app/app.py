5bcc4bd877159cb4e0e96b7d3ea0d524621d66df
vinsky001 <ekibet544@gmail.com>
2026-01-13 20:07:19 +0300

    Add API method to add, get and create a books in a store

--- app/app.py
+++ app/app.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+books = {}
+
+#gets all books
+@app.get("/books")
+def get_all_books(limit: int):
+    if limit:
+        return list(books.values())[:limit]
+    return books
+
+#gets books at a store by ID
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    if not book_id in books:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return books.get(book_id)
+
+@app.post("/books")
+def create_book(book: str, book_id: int):
+    pass
